947e7a6d0d32d2cc33e136c8a8be496f3c886fa9
KpwnZ <22996989+KpwnZ@users.noreply.github.com>
2024-03-13 11:45:39 -0700

    xnu: cleanup

--- hw/arm/xnu.c
+++ hw/arm/xnu.c
@@ -27,6 +27,7 @@
 #include "hw/boards.h"
 #include "hw/arm/xnumem.h"
 #include "hw/arm/xnudt.h"
+#include "hw/arm/d22-idevice.h"
 
 #define DEBUG_XNU_DEVICETREE
 // #undef DEBUG_XNU_DEVICETREE
@@ -81,38 +82,6 @@ void macho_file_highest_lowest(const char *data, hwaddr phys_base,
     *virt_base = align64low(*lowest) & (~0x3fffffff);
 }
 
-static void arm_add_ramdisk_to_devicetree(gchar *dt_data, size_t dt_len, 
-                                          uint64_t ramdisk_addr, uint64_t ramdisk_size)
-{
-    gchar *dt = dt_data;
-    XNUDTProp *prop = NULL;
-    for (size_t i = 0; i < dt_len; i++) {
-        if (strncmp((const char *)dt + i, "MemoryMapReserved-0", kPropNameLength) == 0) {
-            prop = (XNUDTProp *)(dt + i);
-            break;
-        }
-    }
-    strncpy((char *)prop->name, "RAMDisk", kPropNameLength);
-    uint64_t *value = get_member_ptr(XNUDTProp, uint64_t, prop, value);
-    value[0] = ramdisk_addr;
-    value[1] = ramdisk_size;
-}
-
-static void override_platform(gchar *dt_data, size_t dt_len)
-{
-    gchar *dt = dt_data;
-    XNUDTProp *prop = NULL;
-    for (size_t i = 0; i < dt_len; i++) {
-        if (strncmp((const char *)dt + i, "secure-root-prefix", kPropNameLength) == 0) {
-            prop = (XNUDTProp *)(dt + i);
-            break;
-        }
-    }
-    if(prop) {
-        strncpy((char *)prop->name, "no-secure-root-prefix", kPropNameLength);
-    }
-}
-
 static size_t arm_load_xnu_bootargs(struct arm_boot_info *info, AddressSpace *as, MemoryRegion *mem,
                                     uint64_t bootargs_addr, uint64_t virt_base,
                                     uint64_t phys_base, uint64_t top_of_kernel_data,
@@ -131,7 +100,8 @@ static size_t arm_load_xnu_bootargs(struct arm_boot_info *info, AddressSpace *as
     boot_args.deviceTreeP = (void *)dtb_address;
     boot_args.deviceTreeLength = dtb_size;
     boot_args.memSizeActual = 0;
-
+    boot_args.Video.v_baseAddr = 0x41414141;
+    boot_args.Video.v_display = 1;
     rom_add_blob_fixed_as("xnu_boot_args", &boot_args, sizeof(boot_args), bootargs_addr, as);
     return sizeof(boot_args);
 }
@@ -145,9 +115,23 @@ XNUDTNode *arm_load_xnu_devicetree(gchar *blob) {
     return root;
 }
 
+static bool keep_compatible(const char *name) {
+    const char *keep_list[] = {
+        "aic", "aic-timebase", "uart",
+        "disp0"
+    };
+    for (int i = 0; i < sizeof(keep_list) / sizeof(keep_list[0]); i++) {
+        int len = strlen(keep_list[i]);
+        if (strncmp(name, keep_list[i], len) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 static gchar *preprocess_devicetree(
     XNUDTNode *root, uint64_t *dt_len, 
-    uint64_t ramdisk_addr, uint64_t ramdisk_size) {
+    uint64_t ramdisk_addr, uint64_t ramdisk_size, const uint8_t *nvram_proxy_data) {
     // set cpu0's "timebase-frequency" property to freq
     uint64_t freq = 2000000;
     XNUDTNode *node = arm_get_xnu_devicetree_node_by_path(root, "/device-tree/cpus/cpu0");
@@ -201,9 +185,19 @@ static gchar *preprocess_devicetree(
         arm_add_xnu_devicetree_prop(root, "display-scale", sizeof(display_scale), (char *)&display_scale, "/device-tree/chosen");
     }
 
+    prop = arm_get_xnu_devicetree_prop(node, "nvram-proxy-data");
+    memset(prop->value, 0, prop->length);
+    memcpy(prop->value, nvram_proxy_data, 8192);
+    prop = arm_get_xnu_devicetree_prop(node, "nvram-total-size");
+    if (!prop) {
+        uint64_t data = 8192;
+        arm_add_xnu_devicetree_prop(root, "nvram-total-size", sizeof(uint32_t), (char *)&data, "/device-tree/chosen");
+    } else { *(uint32_t *)prop->value = 8192; }
+
     uint32_t data = 1;
     arm_add_xnu_devicetree_prop(root, "security-domain", sizeof(data), (char *)&data, "/device-tree/chosen");
     arm_add_xnu_devicetree_prop(root, "chip-epoch", sizeof(data), (char *)&data, "/device-tree/chosen");
+    data = 0;
     arm_add_xnu_devicetree_prop(root, "debug-enabled", sizeof(data), (char *)&data, "/device-tree/chosen");
 
     node = arm_get_xnu_devicetree_node_by_path(root, "/device-tree/arm-io/aic");
@@ -216,16 +210,12 @@ static gchar *preprocess_devicetree(
 
     node = arm_get_xnu_devicetree_node_by_path(root, "/device-tree/arm-io");
     prop = arm_get_xnu_devicetree_prop(node, "compatible");
-    // memset(prop->value, 0, prop->length);
     for (GList *l = node->children; l != NULL; l = l->next) {
         XNUDTNode *child = (XNUDTNode *)l->data;
         if (!child) break;
         XNUDTProp *name = arm_get_xnu_devicetree_prop(child, "name");
         if (name) {
-            if (strcmp((char *)name->value, "aic") && strcmp((char *)name->value, "aic-timebase")) {
-                if (strncmp((char *)name->value, "uart", 4) == 0) {
-                    continue;
-                }
+            if (!keep_compatible((const char *)name->value)) {
                 XNUDTProp *comp = arm_get_xnu_devicetree_prop(child, "compatible");
                 if (comp) {
                     memset(comp->value, 0, comp->length);
@@ -258,8 +248,11 @@ static gchar *preprocess_devicetree(
     return (gchar *)dtb;
 }
 
+#define DEBUG_XEMU
 int64_t xnu_init_memory(struct arm_boot_info *info,
+                               MachineState *m,
                                XNUDTNode *devicetree,
+                               const uint8_t *nvram_proxy_data,
                                hwaddr *pentry,
                                AddressSpace *as,
 							   MemoryRegion *sysmem,
@@ -269,14 +262,12 @@ int64_t xnu_init_memory(struct arm_boot_info *info,
 #ifdef DEBUG_XEMU
     printf("[*] %s arm_load_macho: %s\n", __PRETTY_FUNCTION__, info->kernel_filename);
 #endif
-
-    int64_t ret = -1;
+    D22IDeviceMachineState *d22 = D22_IDEVICE_MACHINE(m);
     // hwaddr kernel_load_offset = 0;
     hwaddr mem_base = info->loader_start;
     gchar *data = NULL;
     uint8_t *rom_buf = NULL;
     gsize len;
-    gchar *raw_data = data;
     int ismacho = 0;
     uint64_t virt_base = 0;
     uint64_t phys_base = mem_base;
@@ -285,7 +276,7 @@ int64_t xnu_init_memory(struct arm_boot_info *info,
     uint64_t low = 0;
     uint64_t high = 0;
 
-    ret = g_file_get_contents(info->kernel_filename, &data, &len, NULL);
+    g_file_get_contents(info->kernel_filename, &data, &len, NULL);
     if(len <= 0) { exit(0); }
     while (*(uint32_t *)data != 0xFEEDFACF) {
         data += 0x4;
@@ -304,9 +295,7 @@ int64_t xnu_init_memory(struct arm_boot_info *info,
     set_g_virt_base(virt_base);
     set_g_phys_base(info->loader_start);
 
-#ifdef DEBUG_XEMU
     printf("[*] arm_load_macho: low: 0x%llx, high: 0x%llx, size: 0x%llx\n", low, high, size);
-#endif
 
     // (2) copy kernel data to rom_buf
     uint64_t rom_buf_size = align64(high) - low;
@@ -346,11 +335,29 @@ int64_t xnu_init_memory(struct arm_boot_info *info,
         cmd = (struct load_command *)((char *)cmd + cmd->cmdsize);
     }
 
+    // load trust cache
+    hwaddr rom_base = vtop_bases(low, phys_base, virt_base);
+    uint64_t tcaddr = 0;
+    gsize tcsize = 0;
+    gchar *tcdata;
+    g_file_get_contents(d22->trustcache_fn, &tcdata, &tcsize, NULL);
+    gchar *tc_kernel_data = g_malloc0(tcsize + 8);
+    *(uint32_t *)tc_kernel_data = 1;
+    *(uint32_t *)(tc_kernel_data + 4) = 8;
+    memcpy(tc_kernel_data + 8, tcdata, tcsize);
+    tcsize += 8;
+    tcaddr = align64(rom_base - tcsize - 0x4000);
+    rom_add_blob_fixed_as("xnu.trustcache", tc_kernel_data, tcsize, tcaddr, as);
+    g_free(tcdata);
+    g_free(tc_kernel_data);
+    printf("[*] trustcache addr: 0x%llx, size: 0x%lx\n", tcaddr, tcsize);
+    uint64_t tc_dt_prop[2] = { tcaddr, tcsize };
+    arm_add_xnu_devicetree_prop(devicetree, "TrustCache", sizeof(tc_dt_prop), (char *)tc_dt_prop, "/device-tree/chosen/memory-map");
+
     // (3) mask the address
     printf("[*] loader start: 0x%llx\n", info->loader_start);
     pc = vtop_bases(pc, phys_base, virt_base);
 	*pc_addr = pc;
-    hwaddr rom_base = vtop_bases(low, phys_base, virt_base);
     printf("[*] kernel phy base: 0x%llx\n", rom_base);
     rom_add_blob_fixed_as("xnu.kernel", rom_buf, size, rom_base, as);
     g_free(data);
@@ -361,14 +368,7 @@ int64_t xnu_init_memory(struct arm_boot_info *info,
     phys_ptr = (align64(vtop_bases(high, phys_base, virt_base)));
     // uint64_t offset = high;
     // printf("[*] phys_ptr: 0x%llx, dt_addr: 0x%llx\n", phys_ptr, offset);
-
-    // load trust cache
-    // uint64_t tcaddr = 0, tcsize = 0;
-    // gchar *tcdata;
-    // g_file_get_contents("trustcache", &tcdata, &tcsize, NULL);
-    // tcaddr = info->loader_start;
-    // rom_add_blob_fixed_as("xnu.trustcache", tcdata, tcsize, tcaddr, as);
-
+    
     uint64_t ramdisk_addr = 0;
 	gsize ramdisk_size = 0;
     gchar *ramdisk_data = NULL;
@@ -385,7 +385,7 @@ int64_t xnu_init_memory(struct arm_boot_info *info,
     uint64_t dt_len = 0;
 
     XNUDTNode *root = devicetree;
-    gchar *new_dt_data = preprocess_devicetree(root, (uint64_t *)&dt_len, ramdisk_addr, ramdisk_size);
+    gchar *new_dt_data = preprocess_devicetree(root, (uint64_t *)&dt_len, ramdisk_addr, ramdisk_size, nvram_proxy_data);
     rom_add_blob_fixed_as("xnu.dtb", new_dt_data, dt_len, phys_ptr, as);
     g_free(dt_data);
 
